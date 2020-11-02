b5de92bc6c50d4344878d9296cd78836ae48d1c6
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-02 19:11:19 +0500

    adding css

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -7,7 +7,7 @@ function Product() {
   return (
     <div>
        <h1>wellcome to product</h1>
-       <div>
+       <div className="productContainer">
          {Object.keys(Shoes).map(keyName =>{
            const shoe = Shoes[keyName];
            return ( <div key={keyName}> 
