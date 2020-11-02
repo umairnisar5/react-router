e71fd8f6df51732117d468ee6fb473690d41f8a7
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-02 18:33:45 +0500

    using keys method

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Shoes from './../shoes.json';
+
+
 
 function Product() {
   return (
     <div>
        <h1>wellcome to product</h1>
+       <div>
+         {Object.keys(Shoes).map(keyName =>{
+           return ( <div> 
+           <h2>{keyName}</h2>
+           </div> )
+         })}
+       </div>
     </div>
   );
 }
